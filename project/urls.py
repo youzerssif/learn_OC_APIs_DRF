bc1275a80562285c37ee26bd4efd2f90284b1de3
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2022-03-14 12:57:03 +0100

    Change APIView to ModelViewSet and add Read only permission on category

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from shop.views import CategoryAPIView, ProductAPIView
+from rest_framework import routers
+
+from shop.views import CategoryViewset
+
+
+router = routers.SimpleRouter()
+
+router.register('category', CategoryViewset, basename='category')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/category/', CategoryAPIView.as_view()),
-    path('api/product/', ProductAPIView.as_view()),
+    # path('api/category/', CategoryAPIView.as_view()),
+    # path('api/product/', ProductAPIView.as_view()),
+    path('api/', include(router.urls)),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,22 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from .serializers import CategorySerializer, ProductSerializer
 from shop.models import Category, Product
 
 
+class CategoryViewset(ReadOnlyModelViewSet):
+    
+    serializer_class = CategorySerializer
+    
+    def get_queryset(self):
+        return Category.objects.all()
 
-class CategoryAPIView(APIView):
-    
-    def get(self, *arg, **kwargs):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
-    
-class ProductAPIView(APIView):
-    
-    def get(self, *args, **kwargs):
-        
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)
